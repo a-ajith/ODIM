e3219490c96ef1e4b59e7d9e39eafe4b4247f906
Arjun Ajith <arjun.ajith@hpe.com>
2020-12-16 17:11:11 +0530

    added package to the proto files

--- build/generate_rpc_files.sh
+++ build/generate_rpc_files.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # (C) Copyright [2020] Hewlett Packard Enterprise Development LP
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -26,9 +26,21 @@ sudo cp protoc /usr/bin
 cd ../include
 sudo cp -r google /usr/local/include/
 cd
-go get github.com/micro/micro/v3/cmd/protoc-gen-micro@master
-go install google.golang.org/protobuf/cmd/protoc-gen-go
+#export GO111MODULE=on
+#go get github.com/micro/micro/v3
+#cd "$GOPATH"/src/github.com/micro/micro
+#OLD="google.golang.org/grpc v1.27.0"
+#NEW="google.golang.org/grpc v1.26.0"
+#file=go.mod
 
+#sed "s|$OLD|$NEW|g" $file
+#go build -o protoc-gen-micro
+#sudo cp protoc-gen-micro /usr/bin
+#cd
+go install github.com/micro/protoc-gen-micro
+go install google.golang.org/protobuf/cmd/protoc-gen-go
+cd "$GOBIN"
+sudo cp protoc-gen-go protoc-gen-micro /usr/bin
 cd "$current_dir"
 pwd
 
@@ -38,14 +50,13 @@ for entry in ./*
 do
   if [[ "$entry" == *"$sub" ]];
   then
-    dir_name="${entry//.proto}"
-    mkdir "$dir_name"
-    cp "$entry" "$dir_name"
-    cd "$dir_name"
-    protoc -I /usr/local/include/ --proto_path=$GOPATH/src:. --micro_out=. --go_out=. "$entry"
-    cd ..
+#    dir_name="${entry//.proto}"
+#    mkdir "$dir_name"
+#    cp "$entry" "$dir_name"
+#    cd "$dir_name"
+    sudo protoc -I /usr/local/include/ --proto_path=$GOPATH/src:. --micro_out=. --go_out=. "$entry"
+#    cd ..
   else
-    rm -rf "$entry"
+    sudo rm -rf "$entry"
   fi
 done
-
